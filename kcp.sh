7171cf787d987375be605bd61255e46091b5d210
huanter <huanter@126.com>
2017-03-09 05:54:25 +0000

    Modify to i386

--- kcp.sh
+++ kcp.sh
@@ -1,2 +1,2 @@
 cd /root/kcptun/
-nohup ./server_linux_amd64 -l ":29900" -t "127.0.0.1:8388" --crypt aes-128 --key "Errors1937" --mtu 1350 --sndwnd 1024 --rcvwnd 1024 --mode fast --nocomp > /dev/null 2>&1 &
+nohup ./server_linux_386 -l ":29900" -t "127.0.0.1:8388" --crypt aes-128 --key "Errors1937" --mtu 1350 --sndwnd 1024 --rcvwnd 1024 --mode fast --nocomp > /dev/null 2>&1 &
